e7e2a02046577cfbfaaa53c1e23797f09d9c14fc
Bastien Boutonnet <hello@bastienboutonnet.com>
2018-01-13 18:15:03 +0100

    optimised search for reddit link

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def load_and_append_data(new_data, filename, output=False):
     else:
         new_data.to_json('./data/{}.json'.format(filename),
                          orient='records')
+
 @click.command()
 @click.option('--restrict_markets/--no-restrict_markets', default=False)
 @click.argument('limit', type=int)
@@ -69,7 +70,6 @@ def main(restrict_markets, limit, db_file):
         resp = urllib2.urlopen(base_url+'#markets')
         soup = BeautifulSoup(resp, from_encoding=resp.info().getparam('charset'))
 
-        counter = 0
         exchange_list = []
         for link in soup.find_all('a', href=True):
             # if counter == 0: # TODO move this to write restriction as I still wanna create a list of exchanges.
@@ -80,24 +80,23 @@ def main(restrict_markets, limit, db_file):
                     # if link['href'].split('/')[2] not in ALLOWED_EXCHANGES:
                         # pass
                     # else:
-                if exchange_list:
-                    if counter == 0:
-                        logging.info('Looking for Reddit link for: {}'.format(coin.upper()))
-                        # cur_url = 'https://coinmarketcap.com/'+'currencies/'+coin+'#social'
-                        # base_url = 'https://coinmarketcap.com/currencies/nxt/#social'
-                        resp = urllib2.urlopen(base_url+'#social')
-                        soup = BeautifulSoup(resp, from_encoding=resp.info().getparam('charset'))
-                        scripts = soup.find_all("script")
-                        for i in range(len(scripts)):
-                            cur_script = str(scripts[i].string)
-                            match = re.findall('https:\/\/www.reddit.com\/r\/\w+', cur_script)
-                            if match:
-                                logging.info('link found')
-                                logging.info(str(match))
-                                social_url = match
-                                counter += 1
+        if exchange_list:
+            logging.info('Looking for Reddit link for: {}'.format(coin.upper()))
+            # cur_url = 'https://coinmarketcap.com/'+'currencies/'+coin+'#social'
+            # base_url = 'https://coinmarketcap.com/currencies/nxt/#social'
+            resp = urllib2.urlopen(base_url+'#social')
+            soup = BeautifulSoup(resp, from_encoding=resp.info().getparam('charset'))
+            scripts = soup.find_all("script")
+            for i in range(len(scripts)):
+                cur_script = str(scripts[i].string)
+                match = re.findall('https:\/\/www.reddit.com\/r\/\w+', cur_script)
+                if match:
+                    logging.info('link found')
+                    logging.info(str(match))
+                    social_url = match
+
         if len(social_url) > 0:
-            logging.info('Loading reddit subscription data')
+            logging.info('Loading Reddit subscription data')
             req = urllib2.Request(social_url[0]+'/about.json', headers={'User-Agent': 'Mozilla/5.0'})
             opener = urllib2.build_opener()
             try:
@@ -119,10 +118,17 @@ def main(restrict_markets, limit, db_file):
                 logging.info('something FUCKED up with {}'.format(social_url))
                 pass
         else:
-            logging.info('not on allowed exchange or so')
+            logging.info('############# No Reddit found for {}'.format(coin.upper()))
     # save stuff
     logging.info('saving to db')
-    load_and_append_data(subscriptions_data, db_file)
+    dat = load_and_append_data(subscriptions_data, db_file, output=True)
+    # ####### Analysis.
+    # subset df to only coins with allowed exchanged.
+    # possibly implement date range restriction
+    # ## group by coin, exchange, iterate through grouped df,
+    # on the dat object open raw number or pct_change?
+    # calc 2 day avg, week avg
+    # ## start with thresholds like these: 2 day avg is over 3*(week), or today is 2.5*(weekly)
 
 if __name__ == '__main__':
     main()
